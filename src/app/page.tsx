fe8af891761dbe8799f340eafdc94384bac79d86
CarlosPProjects <cg9822@gmail.com>
2024-12-01 16:34:23 +0100

    Add music card list component with carousel functionality and update styles

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,13 @@
 import Container from '@/components/container'
+import MusicCardList from '@/components/music-card-list'
 import React from 'react'
 
 const Home = () => {
   return (
     <Container className='flex-1 flex'>
-      <main className='w-full rounded-3xl bg-gray-200 p-2'>
-        <div className='w-full h-full rounded-3xl bg-green-300 __hero-section'>
-
+      <main className='w-full rounded-3xl bg-gray-200/50 backdrop-blur p-2'>
+        <div className='flex justify-center items-center w-full h-full rounded-3xl bg-green-300 __hero-section'>
+          <MusicCardList />
         </div>
       </main>
     </Container>
---
+++ src/components/music-card-list.tsx
@@ -0,0 +1,34 @@
+import { Card, CardContent } from "@/components/ui/card"
+import {
+  Carousel,
+  CarouselContent,
+  CarouselItem,
+} from "@/components/ui/carousel"
+
+const MusicCardList = () => {
+  return (
+    <Carousel
+      opts={{
+        align: "start",
+        loop: true,
+      }}
+      className="w-full"
+    >
+      <CarouselContent className="-ml-2">
+        {Array.from({ length: 5 }).map((_, index) => (
+          <CarouselItem key={index} className="md:basis-1/2 lg:basis-1/3 pl-2">
+            <div className="p-0">
+              <Card>
+                <CardContent className="flex aspect-square items-center justify-center p-6">
+                  <span className="text-3xl font-semibold">{index + 1}</span>
+                </CardContent>
+              </Card>
+            </div>
+          </CarouselItem>
+        ))}
+      </CarouselContent>
+    </Carousel>
+  )
+}
+
+export default MusicCardList
