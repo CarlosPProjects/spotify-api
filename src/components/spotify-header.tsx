1be145024a79d727c87e8030e94ea48af3772d79
CarlosPProjects <cg9822@gmail.com>
2024-12-03 00:07:06 +0100

    refactor: Update SpotifyHeader layout to center content on small screens and hide title on smaller devices

--- src/components/spotify-header.tsx
+++ src/components/spotify-header.tsx
@@ -3,8 +3,8 @@ import SelectSpotifyFilter from './select-spotify-filter'
 
 const SpotifyHeader = () => {
   return (
-    <header className='w-full flex justify-between items-center p-4'>
-      <span className='block font-semibold'>ipsum</span>
+    <header className='w-full flex justify-center sm:justify-between items-center p-4'>
+      <span className='hidden sm:block font-semibold'>ipsum</span>
       <SelectSpotifyFilter />
     </header>
   )
