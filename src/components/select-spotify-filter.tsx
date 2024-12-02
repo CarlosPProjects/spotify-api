df79cf0d340c4cf1e4c8f1738e80f63490f197cf
CarlosPProjects <cg9822@gmail.com>
2024-12-02 23:18:02 +0100

    refactor: Add form components and validation using react-hook-form; integrate Radix UI label and select components

--- src/components/select-spotify-filter.tsx
+++ src/components/select-spotify-filter.tsx
@@ -1,8 +1,72 @@
-import React from 'react'
+'use client'
+
+import { zodResolver } from "@hookform/resolvers/zod"
+import { useForm } from "react-hook-form"
+import { z } from "zod"
+
+import { Button } from "@/components/ui/button"
+import {
+  Form,
+  FormControl,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "@/components/ui/form"
+
+import {
+  Select,
+  SelectContent,
+  SelectItem,
+  SelectTrigger,
+  SelectValue,
+} from "@/components/ui/select"
+
+const filterSchema = z.object({
+  filter: z
+    .string({
+      required_error: "Please select a filter",
+    })
+})
 
 const SelectSpotifyFilter = () => {
+
+  const form = useForm<z.infer<typeof filterSchema>>({
+    resolver: zodResolver(filterSchema),
+  })
+
+  function onSubmit(data: z.infer<typeof filterSchema>) {
+    console.log(data);
+  }
+
   return (
-    <div>SelectSpotifyFilter</div>
+    <Form {...form}>
+      <form onSubmit={form.handleSubmit(onSubmit)} className="w-2/3 space-y-6">
+        <FormField
+          control={form.control}
+          name="filter"
+          render={({ field }) => (
+            <FormItem>
+              <FormLabel>Email</FormLabel>
+              <Select onValueChange={field.onChange} defaultValue={field.value}>
+                <FormControl>
+                  <SelectTrigger>
+                    <SelectValue placeholder="Select filter" />
+                  </SelectTrigger>
+                </FormControl>
+                <SelectContent>
+                  <SelectItem value="tracks">Top Tracks</SelectItem>
+                  <SelectItem value="tracks">My Playlists</SelectItem>
+                  <SelectItem value="tracks">Top Artists</SelectItem>
+                </SelectContent>
+              </Select>
+              <FormMessage />
+            </FormItem>
+          )}
+        />
+        <Button type="submit">Submit</Button>
+      </form>
+    </Form>
   )
 }
 
